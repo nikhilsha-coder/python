6449c9be8bcdb4ac6c078c7cee01bce58d3e6c24
nikhilsha-coder <75582785+nikhilsha-coder@users.noreply.github.com>
2020-12-06 14:31:32 -0600

    Update testapi.py

--- testapi.py
+++ testapi.py
@@ -7,4 +7,4 @@ for ship in starships['results']:
         e = requests.get(pilot).json()['name']
         print('Pilots:', e)
     if len(ships['pilots'])
-print('This is a test.')hullo my name is nik
+print('This is a test.')
